73804dd56aa2acb909fb05048bc528a6d0a2ac91
pritam dutta <webdevpritam7498@gmail.com>
2024-08-11 01:27:30 +0600

    custom color and font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        heroBg: "#0e1122",
+        para: "rgb(0 0 0 / 80%)",
+        primary: "#06a055",
+      },
+      fontFamily: {
+        primary: ["Poppins", "sans-serif"],
+        secondary: ["Syne", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
