995946cc1977cab4e1d09530322ac181592833c1
pritam dutta <webdevpritam7498@gmail.com>
2024-08-12 17:51:35 +0600

    hero section is complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import heroImg from '../assets/hero.webp';
+import { IoArrowForwardCircleSharp } from "react-icons/io5";
 
 const Hero = () => {
   return (
-    <div id='home' className='h-screen bg-gray-700'>Hero</div>
-  )
-}
+    <section
+      id="home"
+      className="bg-heroBg text-white flex items-center pt-28 md:h-screen"
+    >
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between p-8 overflow-y-hidden gap-12 h-full">
+        {/* left side */}
+        <div className="md:w-1/2">
+        <h1 className="text-4xl font-secondary font-bold mb-4 md:w-3/5 leading-snug">Start Your Journey to Mental Wellness</h1>
+        <p className="text-lg mb-12 md:pr-8">Our team of experienced professionals is committed to helping you navigate life’s challenges with confidence and clarity. </p>
+        <button className="bg-primary text-white py-3.5 px-8 font-medium rounded-md hover:bg-primary/90">
+          <a href="#contact" className="flex gap-1 items-center">
+            <span>Get Started</span>
+            <IoArrowForwardCircleSharp/>
+          </a>
+        </button>
+        </div>
+        {/* right side */}
+        <div className="md:w-1/2 h-full">
+          <img src={heroImg} alt="hero img" className="w-full object-cover" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
